cdb9b28bd0d52829bc590ba90dae6889f45b871a
ustbliubo <ustb_liubo@qq.com>
2016-06-25 22:59:38 +0800

    logging

---
+++ BasicKnowledge/log_test.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+__author__ = 'liubo'
+
+"""
+@version: 
+@author: 刘博
+@license: Apache Licence 
+@contact: ustb_liubo@qq.com
+@software: PyCharm
+@file: log_test.py
+@time: 2016/6/25 22:48
+"""
+
+
+import logging
+logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='test.log',
+                    filemode='w')
+
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warning message')
+logging.error('error message')
+logging.critical('critical message')
+
+if __name__ == '__main__':
+    pass
