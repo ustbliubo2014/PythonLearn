094dcd3e5763747129e2f00fd3cd62e4bb4121d2
liubo-it <liubo@b.360.cn>
2016-10-08 18:27:08 +0800

    decorator

--- BasicKnowledge/tmp.py
+++ BasicKnowledge/tmp.py
@@ -1,22 +1,34 @@
-# encoding: utf-8
-
-"""
-@author: liubo
-@software: PyCharm Community Edition
-@file: tmp.py
-@time: 2016/10/8 18:07
-@contact: ustb_liubo@qq.com
-@annotation: tmp
-"""
-import sys
-import logging
-from logging.config import fileConfig
-import os
-
-reload(sys)
-sys.setdefaultencoding("utf-8")
-fileConfig('logger_config.ini')
-logger_error = logging.getLogger('errorhandler')
-
-if __name__ == '__main__':
-    pass
+# -*- coding:gbk -*-
+'''示例8: 装饰器带类参数'''
+ 
+class locker:
+    def __init__(self):
+        print("locker.__init__() should be not called.")
+         
+    @staticmethod
+    def acquire():
+        print("locker.acquire() called.(static method)")
+         
+    @staticmethod
+    def release():
+        print("  locker.release() called.(no need class)")
+ 
+def deco(cls):
+    '''cls 必须实现acquire和release静态方法'''
+    def _deco(func):
+        def __deco():
+            print("before %s called [%s]." % (func.__name__, cls))
+            cls.acquire()
+            try:
+                return func()
+            finally:
+                cls.release()
+        return __deco
+    return _deco
+ 
+@deco(locker)
+def myfunc():
+    print(" myfunc() called.")
+ 
+myfunc()
+myfunc()
