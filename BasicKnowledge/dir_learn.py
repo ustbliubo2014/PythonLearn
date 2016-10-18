78ae1b198ad85e978ae26c1a53dc363d13402cdc
ustbliubo <ustb_liubo@qq.com>
2016-10-18 22:39:44 +0800

    dir

---
+++ BasicKnowledge/dir_learn.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+__author__ = 'liubo'
+
+"""
+@version: 
+@author: 刘博
+@license: Apache Licence 
+@contact: ustb_liubo@qq.com
+@software: PyCharm
+@file: dir_learn.py
+@time: 2016/10/18 21:49
+"""
+
+import logging
+import os
+import sys
+import glob
+
+# print glob.glob('E:/code/*/*.pyc')
+#
+# print glob.glob('../../../*/*.py')
+#
+# for i in glob.iglob('e:/code/*/*.py'):
+#     print i
+
